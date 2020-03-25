0d77e220951f6805b216080d6a3ca110016fea21
superjkli <superjkli@gmail.com>
2020-03-25 15:50:38 +0900

    halfway

--- bot.py
+++ bot.py
@@ -1,11 +1,45 @@
 import discord
 import youtube_dl
-
+import asyncio
 from discord.ext import commands
 vc = ""
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'))
 bot.remove_command('help')
 token = 'Njg0NjUwMDk1MTgxOTU1MTA0.Xl9Nyg.-tzleGbfV8flXyYo0Xa4d9BFYWg'
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class yt_source(discord.PCMVolumeTransformer):
+    def __init__(self, source, playdata, volume = 0.5):
+        super().__init__(source, volume)
+        self.playdata = playdata
+        self.url = playdata.get('url')
+        self.title = playdata.get('title')
+
+    @classmethod
+    async def get_url_data(cls, url, loop=None, stream=True):
+        loop = loop or asyncio.get_event_loop()
+        playdata = await loop.run_in_executor(None, extract_info_from_ytdl(url, stream))
+        if 'entries' in data:
+            data = data['entries'][0]
 
 @bot.command()
 async def help(ctx):
@@ -21,5 +55,9 @@ async def join(ctx):
 async def disconnect(ctx):
     guild = ctx.message.guild
     await guild.voice_client.disconnect()
+
+@bot.command()
+async def play(ctx, url):
+    pass
 
 bot.run(token)
