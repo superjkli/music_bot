ad5967fff838bc26e4f45cf770a8c87aa9d3f9fa
superjkli <superjkli@gmail.com>
2020-04-15 14:51:23 +0900

    play volume added

--- bot.py
+++ bot.py
@@ -40,6 +40,8 @@ class yt_source(discord.PCMVolumeTransformer):
         playdata = await loop.run_in_executor(None, extract_info_from_ytdl(url, stream))
         if 'entries' in data:
             data = data['entries'][0]
+        filename = data['url']
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 @bot.command()
 async def help(ctx):
@@ -47,17 +49,23 @@ async def help(ctx):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    vc = await channel.connect()
-
-@bot.command()
 async def disconnect(ctx):
-    guild = ctx.message.guild
-    await guild.voice_client.disconnect()
+    await ctx.voice_client.disconnect()
 
 @bot.command()
-async def play(ctx, url):
-    pass
+async def play(ctx, *, url):
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        ctx.voice_client.play(player)    
 
+@bot.command()
+async def volume(self, ctx, volume: int):
+    if ctx.voice_client is None:
+        return await ctx.send("Not connected to vc")
+    ctx.voice_client.source.volume = volume / 100
+    await ctx.send("volume change done")
+
+@play.before_invoke
+async def join():
+    pass
 bot.run(token)
