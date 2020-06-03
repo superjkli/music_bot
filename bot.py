0fe02a52e254f8e374c466fbab0bd0f63cfe4549
superjkli <superjkli@gmail.com>
2020-06-03 15:21:06 +0900

    playlist added

--- bot.py
+++ bot.py
@@ -29,10 +29,11 @@ ffmpeg_options = {
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
-videoqueue = queue.Queue(maxsize = 10)
+videoqueue = queue.Queue(maxsize=10)
 now_playing = ""
 play_author = ""
 voiceclient = None
+get_pl = 0
 class info():
     def __init__(self, context, url):
         self.context = context
@@ -63,6 +64,7 @@ async def help(ctx):
     embed.add_field(name = "disconnect", value = "disconnect from voice", inline = False)
     embed.add_field(name = "volume", value = "change the volume of the music played by the bot 0 - 100, default 50", inline = False)
     embed.add_field(name = "nowplaying", value = "show what is playing now, aliases: np now_playing", inline = False)
+    embed.add_field(name = "playlist", value = "show playlist, aliases: pl playqueue", inline = False)
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -79,7 +81,7 @@ async def play(ctx, *, url):
             temp = info(ctx, url)
             videoqueue.put(temp)
         else:
-            ctx.send("queue full")
+            await tx.send("queue full")
 @bot.command()
 async def volume(ctx, volume: int):
     if ctx.voice_client is None:
@@ -97,6 +99,28 @@ async def skip(ctx):
 @bot.command(aliases=['np', 'now_playing'])
 async def nowplaying(ctx):
     await ctx.send(f"""Now playing: {now_playing}""")
+
+@bot.command(aliases=['playqueue', 'pl'])
+async def playlist(ctx):
+    get_pl = 1
+    if not videoqueue.empty():
+        embed = discord.Embed(title="playlist")
+        i = videoqueue.qsize()
+        number = 0
+        while i > 0:
+            temp = videoqueue.get()
+            title_temp = ytdl.extract_info(temp.url, download=False)
+            if 'entries' in title_temp:
+                title_temp = title_temp['entries'][0]
+            title_temp = title_temp['title']
+            number += 1
+            embed.add_field(name=f"""{number}""", inline=False, value=f"""{title_temp}: \n {temp.url}""")
+            videoqueue.put(temp)
+            i -= 1
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("playlist is empty")
+    get_pl = 0
 
 async def join(ctx):
     if ctx.voice_client is None:
@@ -123,7 +147,7 @@ async def playqueue_check():
     while not bot.is_closed():
         await asyncio.sleep(1)
         if voiceclient is not None:
-            if not voiceclient.is_playing() and not videoqueue.empty():
+            if not voiceclient.is_playing() and not videoqueue.empty() and get_pl == 0:
                 videoinfo = videoqueue.get()
                 await start(videoinfo.context, videoinfo.url)
         
